c61e762525d6556fa8e870f39f49e7a1b0817240
Milad Nekofar <milad@nekofar.com>
2019-07-01 23:20:55 +0430

    Add plugin base file to initialize repository

---
+++ virgool.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Virgool
+Plugin URI: https://github.com/nekofar/virgool
+Description: Virgool lets you publish posts automatically to a Virgool profile.
+Version: 1.0.0
+Author: Milad Nekofar
+Author URI: http://milad.nekofar.com/
+License: GPLv3
+*/
